84a377827048ebe478bbd65e170560b29731d81b
Jacques Celliers (MEA) <jacques.celliers@dimensiondata.com>
2020-08-02 11:43:25 +0200

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jacques.celliers\onedrive - ntt\projects\sportsnow\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
