83bf29f2696d3f6d929117bdc4231564007eb857
Andre <andre@techsonic.co.za>
2020-08-04 10:42:19 +0200

    aaczscac

--- WA/webapp/forms.py
+++ WA/webapp/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class User(forms.Form):
     email = forms.CharField(max_length=100)
     password = forms.CharField(max_length=100)
+
+
+class Event (forms.Form):
+    title = forms.CharField(max_length=100)
+    date_time = forms.CharField(max_length=100)
